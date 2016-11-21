4a3908919e061ec76aa77432e6114db07177f436
domaspiragas <domas.piragas@gmail.com>
2016-11-20 17:39:45 -0700

    Attempt at Dialog Box + Triggers

---
+++ Assets/Scripts/DialogBoxController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DialogBoxController : MonoBehaviour
+{
+
+    public Text dialogTextBox;
+    private string[] m_dialogLines;
+    public float dialogTimer = 3;
+    private float m_dialogTimer;
+    private int m_currentIndex, m_endIndex;
+    private bool m_receivedDialog;
+    private bool m_firstPass;
+    // Use this for initialization
+    void Start()
+    {
+        m_dialogTimer = dialogTimer;
+        m_currentIndex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitDialogBox();
+        }
+        if (m_dialogTimer >= dialogTimer || m_firstPass)
+        {
+            ContinueDialog();
+            m_dialogTimer = 0;
+            m_firstPass = false;
+        }
+
+        m_dialogTimer += Time.deltaTime;
+    }
+    private void ContinueDialog()
+    {
+        if (m_receivedDialog)
+        {
+            if (m_currentIndex <= m_endIndex)
+            {
+                dialogTextBox.text = m_dialogLines[m_currentIndex];
+                m_currentIndex++;
+            }
+            else
+            {
+                ExitDialogBox();
+            }
+        }
+    }
+    public void SetDialog(TextAsset textFile)
+    {
+        m_dialogLines = textFile.text.Split('\n');
+        m_receivedDialog = true;
+        m_endIndex = m_dialogLines.Length - 1;
+        m_firstPass = true;
+    }
+    //Hide and Perform the operations setting up the dialog box for the next time it's needed
+    private void ExitDialogBox()
+    {
+        this.gameObject.SetActive(false);
+        m_receivedDialog = false;
+        m_currentIndex = 0;
+        m_dialogTimer = dialogTimer;
+        dialogTextBox.text = "";
+    }
+}
---
+++ Assets/Scripts/DialogBoxTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DialogBoxTrigger : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public TextAsset dialogText;
+    public Image dialogImage;
+    public Sprite newSprite;
+    private DialogBoxController m_dialogController;
+    void Start()
+    {
+        m_dialogController = dialogBox.GetComponent<DialogBoxController>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (!dialogBox.gameObject.activeSelf)
+        {
+            m_dialogController.SetDialog(dialogText);
+            dialogImage.GetComponent<Image>().sprite = newSprite;
+            dialogBox.gameObject.SetActive(true);
+        }
+    }
+
+}
