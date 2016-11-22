1ab0e415de0b065fe1f10dbec58e1caddcb30b2a
domaspiragas <domas.piragas@gmail.com>
2016-11-21 19:57:18 -0700

    Main Menu art applied

---
+++ Assets/Scripts/ButtonHover.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Sprite nonHover;
+    public Sprite hover;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        this.gameObject.GetComponent<Image>().sprite = hover;
+        Debug.Log("in");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        this.gameObject.GetComponent<Image>().sprite = nonHover;
+        Debug.Log("out");
+    }
+}
